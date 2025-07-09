759d95f8cfcb3e40bc12b465e497602d8cb7cf89
Prem Narayankar <premnarayankar036@gmail.com>
2025-07-10 00:04:03 +0530

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="text-center text-sm text-gray-500 py-4 border-t bg-white">
+      © {new Date().getFullYear()} Prem Narayankar. All rights reserved.
+    </footer>
+  );
+}
